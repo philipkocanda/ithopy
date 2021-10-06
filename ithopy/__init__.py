af43ec1f5ef6030b6cac765d0d3614c8fba10e70
Philip Kocanda <philip@kocanda.nl>
2021-10-06 23:16:37 +0200

    pypi stuff

---
+++ ithopy/__init__.py
@@ -0,0 +1,4 @@
+from ithopy.message import Message
+from ithopy.payload import Payload
+from ithopy.devices.hru_device import HruDevice
+from ithopy.devices.hru_message_builder import HruMessageBuilder
--- setup.py
+++ setup.py
@@ -22,3 +22,29 @@ setup(
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
+
+
+from distutils.core import setup
+setup(
+  name = 'ithopy',
+  packages = ['ithopy'],
+  version = '0.1',
+  license='MIT',
+  description = 'IthoPy is a library to communicate with I2C Itho devices',   # Give a short description about your library
+  author='Philip Kocanda',
+  author_email='philip@kocanda.nl',
+  url='https://github.com/philipkocanda/ithopy',
+  download_url = 'https://github.com/philipkocanda/ithopy/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['itho', 'i2c'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
